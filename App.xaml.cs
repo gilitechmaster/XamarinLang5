c8e45c6dfacc6724f999d56c69428462db7e32dd
길이스타트업 <51491577+gilitechmaster@users.noreply.github.com>
2020-12-20 00:24:23 +0900

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace lang6
+{
+    public partial class App : Application
+    {
+        public static string FolderPath { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new NavigationPage(new Page2());
+        }
+    }
+}
